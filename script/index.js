962ffde9534587ff4c3c13155f621fb8d6815d0c
winbigcoms <bigcoms6290@gmail.com>
2020-04-09 12:40:39 +0900

    네비게이션리스트 에 포커싱이 되어있을 때, 방향키 위아래로 	제어 가능하도록 만들었습니다.

--- script/index.js
+++ script/index.js
@@ -54,31 +54,45 @@ closeNaviBtn.addEventListener("click", function (e) {
 closeNaviBtn.addEventListener("keydown",function(e){
     //키보드 키를 누를때 이벤트 발생하도록 이벤트 생성
     // 이벤트가 일어나는 요소의 기본이벤트를 막기
-    if(e.keyCode == 9){
+    if(e.keyCode == 9 || e.keyCode == 40 ){
         e.preventDefault();
         //만약 눌러진 키가 tab이라면
-        var firstNaviItem = document.querySelector(".navigationItems");
-        firstNaviItem.focus();
+        // var firstNaviItem = document.querySelectorAll(".navigationItems");
+        navigationItems[0].focus();
         //첫 네비게이션 요소에 포커스 주기
-    }else if(e.keyCode == 13 || e.keyCode == 27){
+    }else if(e.keyCode == 13 || e.keyCode == 27 || e.keyCode == 39){
+        // 만약 눌러진 키가 enter(13) 혹은 esc(27) 혹은 오른쪽 화살표(39) 이라면
         navigationBox.classList.remove("openNavi");
+        //클래스제거 
         menuList[0].focus();
+        //메뉴 리스트의 첫 번째 객체로 포커스 주기
+    }else if(e.keyCode == 38){
+        // 만약 눌린 키가 윗 방향 화살표(38) 이라면
+        var targetOn =e.currentTarget.previousElementSibling.lastElementChild;
+        // e.currentTarget 현제 이벤트가 발생한 태그에서
+        // previousElementSibling html상에서 이전에 존재하는 형재 태그 중
+        // lastElementChild 마지막 자식요소를 선택
+
+        targetOn.childNodes[0].focus();
+        // lastElemtChild 는 li라서 포커스가 안되요. 그래서 그 자식요소인 a 태그를 선택
+        // childNodes 는 배열로 선택되니까 첫 번째 자식요소는 0번이 되겠죠?
+        // .focus() 그거에 포커스를 준다.
     }
 })
 
 
 // 네비게이션 클릭 
-for(var i =0; i < navigationItems.length; i++){
+for(var i =0; i < navigationItems.length; ++i){
+    console.log(i)
     // 변수 i는 0인데 , navigationItems 즉 navigationItems이라는 클래스를 갖는 html태그들의 집합인
     //navigationItems의 내부 아이템들의 갯수가 변수 i보다 크면 아래의 코드를 실행한 후 코드가 끝나면
     // i++ 즉 i에 1을 더해준다.
-    
     
     // 최초클릭확인 최초의 클릭이 되어있는지 확인하는 변수, 
     var isThemeOn = false;
 
 
-    // 클릭 이벤트
+    // 네비게이션 메뉴 클릭 시 색들어가는 이벤트
     navigationItems[i].addEventListener("click",function(e){
         // navigationItems의 모든 아이템들에 click이벤트를 추가해준다. 이 클릭이벤트는 아래와 같다
 
@@ -105,6 +119,49 @@ for(var i =0; i < navigationItems.length; i++){
         //추가후엔 isTemeOn의 값을 true로 변경해준다.
         //이러면 다른 네비게이션 아이템을 클릭했을때 if문으로 넘어가게 된다.
         // 즉 최초에 한번만 실행된다는 이야기
+    });
+    // 메뉴 리스트에서 아래 방향키 누르면 다음 메뉴로 이동
+    
+    navigationItems[i].addEventListener("keydown",function(e){
+        // 네비게이션에 키를 누를때 이벤트가 발생하는 이벤트를 걸어준다.
+
+        if(e.keyCode == 40){
+            //만약 눌린 키가 아래방향키(40) 이라면
+            var nextTarget =e.currentTarget.parentNode.nextElementSibling;
+            // e.currentTarget 현제 이벤트가 발생한 태그에서
+            // parentNode html상에서 부모 태그에서
+            // nextElementSibling 부모 태그의 형제 레벨의 태그에서 부모 다음에 존재하는
+            // 태그를 선택
+
+            if( !nextTarget ){
+                //만약 위에서 선택한 태그가 존재하지 않으면 ! > 없다. not 의 의미에요
+                closeNaviBtn.focus();
+                //닫기버튼에 포커스
+            }
+            else if(nextTarget){
+                // 만약 존재한다면 
+                nextTarget.childNodes[0].focus(); 
+                //위에서 선언한 태그의 자식요소 중 첫 번째 
+                //여기선 a태그가 되겠죠? 거기에 포커싱
+            } 
+        }
+        if(e.keyCode == 38){
+            // 만약 눌린 키가 위화살표 (38)라면
+            var prevTarget = e.currentTarget.parentNode.previousElementSibling;
+            // e.currentTarget 현제 이벤트가 발생한 태그에서
+            // parentNode html상에서 부모 태그에서
+            // previousElementSibling 부모 태그의 형제 레벨의 태그에서 부모 이전에 존재하는
+            // 태그를 선택
+            if( !prevTarget){
+                //존재하지않으면
+                closeNaviBtn.focus();
+                //버튼에 포커싱
+            }else if(prevTarget){
+                //존재하면
+                prevTarget.childNodes[0].focus();
+                //위에서 선언한 태그의 자식태그 인 a태그에 포커싱
+            }
+        }
     })
 }
 
